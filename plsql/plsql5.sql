de078345cc7a9a3281f6239c36d523fb2582219a
prafulpathare <patharepraful@gmail.com>
2020-09-19 13:26:57 +0000

    plsql5 complete

--- plsql/plsql5.sql
+++ plsql/plsql5.sql
@@ -1,8 +1,8 @@
 SET SERVEROUTPUT ON
 SET FEEDBACK off
 SET VERIFY off
-truncate table school_test;
 
+-- QUESTION 1
 DECLARE
 	CURSOR C1 IS SELECT * FROM SCHOOL;
 	X SCHOOL%rowtype;
@@ -28,20 +28,92 @@ BEGIN
 			4, 'C',
 			'FAIL') into grade from dual;
 		end if;
---	dbms_output.put_line(X.name||' '||total||' '||perc||' '||grade);
+		dbms_output.put_line(X.name||' '||total||' '||perc||' '||grade);
 		insert into school_test values(X.roll_no, total, perc, grade);
 	END LOOP;
 	close C1;
 END;
 /
 
+-- QUESTION 2
 DECLARE
 	CURSOR C1 IS SELECT * FROM PLSQLFIVE_CUSTOMER;
-	X C1%ROWTYPE;
+	used_units plsqlfive_customer.current_read%type;
+	rate number(3,2);
+	amount number(8,2);
+	surcharge number(3,2);
+	excise number(8,2);
+	net number(10,2);
 BEGIN
-	FOR X IN C1 LOOP
-	--	DBMS_OUTPUT.PUT_LINE(X.meter_type||' '||X.meter||' '||X.current_read||' '||X.customer_type);
-		
+	for x in C1 LOOP
+		used_units := x.current_read - x.prev_read;
+		select decode(x.customer_type,
+			'A', 1, 'I', 1.25, 'C', 1.5,'R', 1.3
+		) into rate from dual;
+		amount := rate * used_units;
+
+		if x.meter_type = 'T' then	surcharge := 0.05;
+		elsif x.meter_type = 'S' then	surcharge := 0.1;
+		end if;
+
+		excise := (amount + surcharge) * 0.3;
+		net := amount + surcharge + excise;
+
+		insert into plsqlfive_two_output values (x.meter_no, used_units, amount, surcharge, excise, net);
+		DBMS_OUTPUT.PUT_LINE(x.meter_no||' '||used_units||' '||amount||' '||surcharge||' '||excise||' '||net);
 	END LOOP;
 END;
+/
+
+-- QUESTION 3
+declare
+	cursor c1 is select * from pl_5_3_main;
+	cursor c2 is select prd_code, sum(qty) summ from pl_5_3_main group by prd_code order by prd_code;
+	rate number(2);
+	total number(8);
+begin
+	-- total purchase for each cx
+	for x in c1 loop
+		-- pl_5_3_main
+		total := 0;
+		select decode(x.prd_code, 0, 15, 1, 35, 2, 42, 3, 51, 4, 60) into rate from dual;
+		total := rate * x.qty;
+		dbms_output.put_line('Total of Customer '||x.cx_code||'      Rs.'||total);
+		insert into pl_5_3_out_1 values (x.cx_code, total);
+	end loop;
+
+	-- total sales per product
+	for y in c2 loop	
+		select decode(y.prd_code, 0, 15, 1, 35, 2, 42, 3, 51, 4, 60) into rate from dual;
+		total := rate * y.summ;
+		dbms_output.put_line('Total sale of Product '||y.prd_code||' is Rs.'||total);
+		insert into pl_5_3_out_2 values (y.prd_code, total);
+	end loop;
+end;
+/
+
+-- QUESTION 4
+declare
+	cursor C1 is select * from pl_5_4_employee;
+	DA number(6,2);
+	HRA number(6,2) := 0;
+	GROSS number(7,2);
+begin
+	for x in C1 loop
+		DA := x.basic * 0.35;
+
+		hra := 0;
+		if x.category = 'W' then
+			if (x.basic*0.15) < 250 then hra := x.basic*0.15; end if;
+		elsif x.category = 'J' then
+			if(x.basic*0.15) < 1000 then hra := x.basic*0.15; end if;
+		elsif x.category = 'S' then
+			if(x.basic*0.15) < 3000 then hra := x.basic*0.15; end if;
+		end if;
+
+		GROSS := x.basic + DA + hra;
+		dbms_output.put_line(x.empno||'   '||GROSS);
+		insert into pl_5_4_output values (x.empno, GROSS);
+	end loop;
+end;
 /
---
+++ plsql/plsql6.sql
@@ -0,0 +1,14 @@
+SET SERVEROUTPUT ON
+SET VERIFY OFF
+SET FEEDBACK OFF
+
+DECLARE
+	amt number(15);
+	amt_str varchar(100) := '';
+BEGIN
+	amt := &amount;
+	dbms_output.put_line(to_char(amt));
+	select to_char(to_date(amt, 'j'), 'jsp') into amt_str from dual;
+	dbms_output.put_line(amt_str);
+END;
+/
